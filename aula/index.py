2f01930ff06b799199ad4908faa5b087014c0847
PedroJanneo <133059965+PedroJanneo@users.noreply.github.com>
2024-03-13 19:39:12 -0300

    Add files via upload

--- aula/index.py
+++ aula/index.py
@@ -3,7 +3,13 @@ from flask import Flask, render_template
 # a varial abaixo é a variavel
 # que roda a aplicacao (todos
 #os objetos de projeto depende dela)
+#pip install flask
 
+class Produto:
+    def __init__(self, nome_prod, marca_prod, preco_prod):
+            self.nome = nome_prod 
+            self.marca = marca_prod
+            self.preco = preco_prod
 
 app = Flask(__name__)
 
@@ -14,6 +20,13 @@ def ola():
 
 @app.route('/lista')
 def lista():
-    return render_template('lista.html')
+    # a linha abaixo instancia um novo produto 
+    prod01 = Produto("Camisa", "NIKE", "R$ 300,00")
+    prod02 = Produto("Blusa", "Lacoste", "R$ 255,00")
+    prod03 = Produto("Calça", "Oakley", "r$ 200,00")
+    
+    
+    produtos_cadastrados = [prod01, prod02, prod03]
+    return render_template('lista.html', descricao = "Aqui estão seus produtos cadastrados", lista_prod = produtos_cadastrados)
 
 app.run()
