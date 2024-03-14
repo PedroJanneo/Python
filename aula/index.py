71b3fb1a7fb28f37904ce3a6db3dc20526e445ce
PedroJanneo <133059965+PedroJanneo@users.noreply.github.com>
2024-03-13 21:48:35 -0300

    Add files via upload

--- aula/index.py
+++ aula/index.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 # a varial abaixo é a variavel
 # que roda a aplicacao (todos
@@ -11,6 +11,14 @@ class Produto:
             self.marca = marca_prod
             self.preco = preco_prod
 
+    # a linha abaixo instancia um novo produto 
+prod01 = Produto("Camisa", "NIKE", "R$ 300,00")
+prod02 = Produto("Blusa", "Lacoste", "R$ 255,00")
+prod03 = Produto("Calça", "Oakley", "r$ 200,00")
+    
+    
+produtos_cadastrados = [prod01, prod02, prod03]
+
 app = Flask(__name__)
 
 @app.route('/inicio')
@@ -20,13 +28,25 @@ def ola():
 
 @app.route('/lista')
 def lista():
-    # a linha abaixo instancia um novo produto 
-    prod01 = Produto("Camisa", "NIKE", "R$ 300,00")
-    prod02 = Produto("Blusa", "Lacoste", "R$ 255,00")
-    prod03 = Produto("Calça", "Oakley", "r$ 200,00")
     
-    
-    produtos_cadastrados = [prod01, prod02, prod03]
     return render_template('lista.html', descricao = "Aqui estão seus produtos cadastrados", lista_prod = produtos_cadastrados)
+
+
+
+@app.route('/cadastrar')
+def cadastrar_produto():
+     return render_template('cadastrar.html')
+
+@app.route('/adicionar',methods=['POST',])
+def adicionar_produto():
+     nome_prod = request.form['txtName']
+     marca_prod = request.form['txtMarca']
+     preco_prod = request.form['txtPreco']
+
+     novo_produto= Produto(nome_prod,marca_prod,preco_prod)
+
+     produtos_cadastrados.append(novo_produto)
+
+     return render_template("lista.html", descricao = 'Novo produto cadastrado', lista_prod = produtos_cadastrados)
 
 app.run()
